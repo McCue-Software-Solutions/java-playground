65a5e78b6d0dbe3b0ed45182a2e4f4423c9834ab
LeonardoA77 <Lemniscate4356@Knights.ucf.edu>
2023-07-10 23:53:27 +0000

    Delete it

--- ui/frontend/MenuItem.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './MenuItem.module.css';
-
-const MenuItem: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
-  <div className={styles.container}>{children}</div>
-);
-
-export default MenuItem;
