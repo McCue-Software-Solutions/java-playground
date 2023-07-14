fc4328e0af64ab486603681051db67905096998b
LeonardoA77 <Lemniscate4356@Knights.ucf.edu>
2023-07-14 17:22:38 +0000

    modified:   server/package.json 	modified:   server/src/app.controller.ts 	modified:   server/src/app.module.ts 	new file:   server/src/engine/engine.controller.spec.ts 	new file:   server/src/engine/engine.module.ts 	new file:   server/src/engine/engine.service.spec.ts 	new file:   server/src/engine/engine.service.ts 	modified:   server/yarn.lock

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EngineModule } from './engine/engine.module';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [EngineModule],
 })
 export class AppModule {}
